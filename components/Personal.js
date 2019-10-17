42563307b5911658b81719852ba55b2ecee9473a
AKH000 <andoniabedul@gmail.com>
2019-10-17 22:40:42 +0200

    Changing colors and other things

--- components/Personal.js
+++ components/Personal.js
@@ -4,7 +4,7 @@ const Personal = (props) => {
   const { location, name, description } = props; 
   return (
     <div className="personal">
-      <h1 className="personal-name">Hola, mi nombre es {name}</h1>
+      <h1 className="personal-name">{name}</h1>
       <h3 className="personal-location">📍{location.city}, {location.country}</h3>
       <h3 className="personal-description">{description}</h3>
     </div>
